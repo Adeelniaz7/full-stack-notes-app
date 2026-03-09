34be04b6ab10e5d7abe3b5596b5452ae2e142d43
Adeelniaz7 <ADEEL7Niaz@gmail.com>
2026-03-10 01:26:48 +0500

    Initial commit

---
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import Profileinfo from '../Cards/Profileinfo'
+import { useNavigate } from "react-router-dom";
+import Searchbar from '../Searchbar/Searchbar';
+
+const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  }
+
+  const handleSearch = () => {
+    if (searchQuery) {
+      onSearchNote(searchQuery);
+    }
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+    handleClearSearch();
+  }
+
+  return (
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+      <div className='flex-1 text-left'>
+        <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
+      </div>
+
+      <div className='flex-1 flex justify-center'>
+        <Searchbar
+          value={searchQuery}
+          onChange={({ target }) => {
+            setSearchQuery(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
+      </div>
+
+      <div className='flex-1 flex justify-end'>
+        <Profileinfo userInfo={userInfo} onLogout={onLogout} />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
